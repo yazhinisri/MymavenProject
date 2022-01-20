94c3bb3ed960c0675362ce6c33cd123875b0765d
Origin <yazhinisri@gmail.com>
2022-01-20 22:08:18 +0530

    test

---
+++ src/test/java/com/mavendemo/demoprejectmaven/AppTest.java
@@ -0,0 +1,21 @@
+package com.mavendemo.demoprejectmaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("I am first testcase");
+        assertTrue( true );
+    }
+}
